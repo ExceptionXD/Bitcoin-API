afb02c8d0e37597f34819b5d571e4bc6a788f856
ExceptionXD <kumarsingh14abhishek@gmail.com>
2020-04-29 21:45:00 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,96 @@
+
+setInterval(getBitcoins , 3000);
+
+function getBitcoins()
+{
+  let xhr  = new XMLHttpRequest();
+  xhr.open('GET','https://api.coindesk.com/v1/bpi/currentprice.json',true);
+  xhr.onload = function(){
+
+    if(this.status===200)
+    {
+      console.log(this.response);
+      var price = JSON.parse(this.response);
+      console.log(price);
+      console.log(price.bpi.USD.rate)
+      var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['USD', 'GBP', 'EUR'],
+        datasets: [{
+            label: 'Price of Bitcoins',
+            data: [`${price.bpi.USD.rate_float}`,`${price.bpi.GBP.rate_float}`,`${price.bpi.EUR.rate_float}`],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+    }
+
+  }
+
+
+
+  xhr.send();
+}
+
+
+
+
+
+// var ctx = document.getElementById('myChart');
+// var myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             }]
+//         }
+//     }
+// });
